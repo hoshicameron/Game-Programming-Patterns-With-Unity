ec6a25694d488cc6b28a55d17d7b32045805034c
hoshicameron <hoshicameron@gmail.com>
2022-08-28 17:56:03 +0430

    4th Commit Implement Event Bus

---
+++ Assets/GPP/Scripts/EventBus/RaceEventBus.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+namespace GameProgrammingPattern.EventBus
+{
+    public class RaceEventBus
+    {
+        private static readonly
+            IDictionary<RaceEventType, UnityEvent>
+            Events = new Dictionary<RaceEventType, UnityEvent>();
+
+        public static void Subscribe
+            (RaceEventType eventType, UnityAction listener) {
+
+            UnityEvent thisEvent;
+
+            if (Events.TryGetValue(eventType, out thisEvent)) {
+                thisEvent.AddListener(listener);
+            }
+            else {
+                thisEvent = new UnityEvent();
+                thisEvent.AddListener(listener);
+                Events.Add(eventType, thisEvent);
+            }
+        }
+
+        public static void Unsubscribe
+            (RaceEventType eventType, UnityAction listener) {
+
+            UnityEvent thisEvent;
+
+            if (Events.TryGetValue(eventType, out thisEvent)) {
+                thisEvent.RemoveListener(listener);
+            }
+        }
+
+        public static void Publish(RaceEventType type) {
+
+            UnityEvent thisEvent;
+
+            if (Events.TryGetValue(type, out thisEvent)) {
+                thisEvent.Invoke();
+            }
+        }
+
+
+    }// class
+}// namespace
---
+++ Assets/GPP/Scripts/EventBus/RaceEventType.cs
@@ -0,0 +1,7 @@
+namespace GameProgrammingPattern.EventBus
+{
+    public enum RaceEventType
+    {
+        COUNTDOWN, START, RESTART, PAUSE, STOP, FINISH, QUIT
+    }
+}
